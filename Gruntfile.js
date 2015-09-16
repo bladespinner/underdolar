559b45dd41e0a46f52b6728ef84314230a93c489
bladespinner <bladespinner@gmail.com>
2015-09-16 21:59:32 +0300

    Init test

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,57 @@
 module.exports = function(grunt) {
     grunt.initConfig({
+        settings: {
+            app: '.',
+            testRoot: '<%= settings.app %>/spec',
+            unit: '<%= settings.testRoot %>/unit',
+            testBuild: '<%= settings.testRoot %>/testBuild',
+            src: '<%= settings.app %>/src',
+            srcLib: '<%= settings.src %>/lib',
+            bowerComponents: '<%= settings.app %>/bower_components'
+        },
+        copy: {
+            libs: {
+                files: [
+                    {expand: true, cwd:'<%= settings.bowerComponents %>/requirejs/', src: ['require.js'], dest: '<%= settings.srcLib %>/', filter: 'isFile'},
+                ]
+            },
+            unit: {
+                files: [
+                    {expand: true, cwd:'<%= settings.src %>/', src: ['**'], dest: '<%= settings.testBuild %>/'},
+                ],
+            }
+        },
+        karma: {
+          unit: {
+            configFile: 'karma.conf.js'
+          }
+        },
         jasmine: {
             unit: {
-                src: 'src/**/*.js',
+                src: '<%= settings.testBuild %>/**/*.js',
                 options: {
-                    specs: 'spec/*Spec.js',
-                    template: require('grunt-template-jasmine-requirejs')
+                    specs: '<%= settings.unit %>/**/*Spec.js',
+                    template: require('grunt-template-jasmine-requirejs'),
+                    templateOptions: {
+                        requireConfigFile: 'src/main.js'
+                    }
                 }
             }
         }
     });
     
-    // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-jasmine');
+    grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-contrib-copy');
     
-    // Default task(s).
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('build:unit', [
+        //'copy:libs',
+        'copy:unit',
+        'karma-unit'
+    ]);
+    
+    grunt.registerTask('unit', [
+        'build:unit',
+        'jasmine:unit'
+    ]);
 };
