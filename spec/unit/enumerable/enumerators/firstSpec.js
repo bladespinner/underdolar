f79c169d99fc8a3b18727597ed70aa7aa0728127
Mihail Gospodinov <mihail@bladespinner.bladespinner>
2015-10-11 02:50:44 -0400

    Add first unit tests

---
+++ spec/unit/enumerable/enumerators/firstSpec.js
@@ -0,0 +1,45 @@
+define(['enumerable/main', 'iterator/arrayIterator'], function (Enumerable, ArrayIterator) {
+    describe('First', function () {
+        /**
+         * TODO: ?Mock Enumerable#first dependencies?
+         */
+        
+        it('should return furst object of enumerable', function () {
+            var data = [
+                {name: 'Ivan', age: 25},
+                {name: 'Georgi', age: 23},
+                {name: 'Pesho', age: 12},
+                {name: 'Smith', age: 9},
+                {name: 'Donald', age: 34}
+            ],
+            dataIterator = new ArrayIterator(data),
+            dataEnumerable = new Enumerable(dataIterator);
+            
+            var result = dataEnumerable.first()
+            
+            expect(result).toEqual({
+                name: 'Ivan',
+                age: 25
+            });
+        });
+        
+        it('should return undefined if enumerable is empty', function () {
+            var data = [
+                {name: 'Ivan', age: 25},
+                {name: 'Georgi', age: 23},
+                {name: 'Pesho', age: 12},
+                {name: 'Smith', age: 9},
+                {name: 'Donald', age: 34}
+            ],
+            dataIterator = new ArrayIterator(data),
+            dataEnumerable = new Enumerable(dataIterator);
+            
+            
+            var result = dataEnumerable.findWhere({
+                name: 'Darwin' 
+            });
+            
+            expect(result).toEqual(undefined);
+        });
+    });
+});
