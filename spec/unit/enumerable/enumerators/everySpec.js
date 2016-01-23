721e805cf5525cc9abe618ae1a0bd30c26d16d9d
Mihail Gospodinov <mihail@bladespinner.bladespinner>
2016-01-23 17:59:24 -0500

    Fix every spec.

--- spec/unit/enumerable/enumerators/everySpec.js
+++ spec/unit/enumerable/enumerators/everySpec.js
@@ -1,60 +1,51 @@
-define(['enumerable/enumerators/every', 'test/mock/enumerable'], function (every, enumMock) {
-    describe('Every enumerator', function () {
-        var everyMocked,
-            EnumerableMock = enumMock.EnumerableMock;
-        
-        beforeEach(function () {
-            var enumerable = new EnumerableMock();
-            everyMocked = every.bind(enumerable);
-        })
-        
-        it('should return false when condition is not true for all values', function () {
-            enumMock.setData([
-                {name: 'Ivan', age: 25},
-                {name: 'Georgi', age: 23},
-                {name: 'Pesho', age: 12},
-                {name: 'Smith', age: 9},
-                {name: 'Donald', age: 34},
-            ]);
+define(['enumerable/main', 'iterator/arrayIterator'], 
+    function (Enumerable, ArrayIterator) {
+        describe('Every', function () {
+            var records = [
+                    {name: 'Ivan', age: 25},
+                    {name: 'Georgi', age: 23},
+                    {name: 'Pesho', age: 12},
+                    {name: 'Smith', age: 9},
+                    {name: 'Donald', age: 34}
+                ],
+                recordIterator = new ArrayIterator(records),
+                recordEnumerator = new Enumerable(recordIterator);
             
-            var result = everyMocked(function (person) {
-                return person.age > 15;
+            it('should return false when condition is not true for all values', function () {
+                var result = recordEnumerator.every(function (person) {
+                    return person.age > 15;
+                });
+                
+                expect(result).toEqual(false);
             });
             
-            expect(result).toEqual(false);
-        });
-        
-        it('should return true when condition is true for all values', function () {
-            enumMock.setData([
-                {name: 'Ivan', age: 25},
-                {name: 'Georgi', age: 23},
-                {name: 'Pesho', age: 12},
-                {name: 'Smith', age: 9},
-                {name: 'Donald', age: 34},
-            ]);
-            
-            var result = everyMocked(function (person, index) {
-                return person.age > 0;
+            it('should return true when condition is true for all values', function () {
+                var result = recordEnumerator.every(function (person, index) {
+                    return person.age > 0;
+                });
+                
+                expect(result).toEqual(true);
             });
             
-            expect(result).toEqual(true);
-        });
-        
-        it('should check if values are truthly if no callback', function () {
-            enumMock.setData([true, {}, 128, 'greetings']);
-            var result = everyMocked();
-            expect(result).toEqual(true);
+            it('should check if values are truthly if no callback', function () {
+                var truthyIterator = new ArrayIterator([true, {}, 128, 'greetings']),
+                    truthyEnumerator = new Enumerable(truthyIterator);
+                
+                expect(truthyEnumerator.every()).toEqual(true);
+                
+                var falsyIterator = new ArrayIterator([false, undefined, null, true, {}, '55s']),
+                    falsyEnumerator = new Enumerable(falsyIterator);
+                
+                expect(falsyEnumerator.every()).toEqual(false);
+                
+            });
             
-            enumMock.setData([false, undefined, null, true, {}, '55s']);
-            result = everyMocked();
-            expect(result).toEqual(false);
-            
+            it('should return true for no values', function () {
+                var emptyIterator = new ArrayIterator([]),
+                    emptyEnumerator = new Enumerable(emptyIterator);
+                    
+                expect(emptyEnumerator.every()).toEqual(true);
+            });
         });
-        
-        it('should return true for no values', function () {
-            enumMock.setData([]);
-            var result = everyMocked();
-            expect(result).toEqual(true);
-        });
-    });
-});
+    }
+);
